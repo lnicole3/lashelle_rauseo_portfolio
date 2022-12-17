bf5fab591a6b606939b3b93200c0d92d3c43fa24
LaShelle Rauseo <lrauseo3@gmail.com>
2022-12-16 19:36:15 -0600

    preparation for railway deployment

--- src/App.jsx
+++ src/App.jsx
@@ -24,19 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "LaShelle Rauseo",
+  title: "Web Developer & Visual Artist",
+  email: "webDevLRauseo@gmail.com",
+  gitHub: "https://github.com/lnicole3",
+  linkedIn: "https://www.linkedin.com/in/lashelle-rauseo/",
 };
 
-const primaryColor = "#4E567E";
-const secondaryColor = "#D2F1E4";
+const primaryColor = "#000814";
+const secondaryColor = "#f2f4f3";
 
 const App = () => {
   return (
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,16 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a creative Full Stack Web Developer with extensive experience working cross-functionally with diverse teams and stakeholders. I am a highly dedicated team player who thrives when building relationships and solving problems. I am excited to combine my coding skills and creativity to contribute to companies that are using technology to enhance the future.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Languages: SQL, Python, JavaScript, HTML, CSS, ",
+  "Software: MongoDB, CodePen, PostgreSQL Github,Adobe Photoshop, Figma, Sketch, Canva",
+  "Frameworks: React, Vue, Express, Flask, Node",
 ];
 
 /**
@@ -45,7 +42,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in creative ways to drive innovation. By leveraging my web development skills, I continually look for new and better ways to make tech helpful, fun, and accessible to all.";
 
 const About = () => {
   return (
